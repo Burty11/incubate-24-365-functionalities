0091cbc37ac214cfab5bd6a87f0bae6f20c99f2a
Burty11 <142454463+Burty11@users.noreply.github.com>
2023-11-01 14:21:26 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,13 +1,43 @@
-        print(f"Random Number: {item}")
+    const data = generateRandomData();
+    });
+    return data;
+function generateRandomData() {
+
+main();
+        data.push(Math.floor(Math.random() * 100) + 1);
+function main() {
+
+}
+    const data = [];
+    }
+        console.log("Random Number: " + item);
+
+}
+    for (let i = 0; i < 10; i++) {
+    data.forEach(item => {    data = [random.randint(1, 100) for _ in range(10)]
+
     for item in data:
 
-    data = generate_random_data()
-
-import random
-if __name__ == "__main__":
 def main():
-    data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
     main()
-
+def generate_random_data():
     return data
+
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+
+    data = generate_random_data()
+import random    data = generate_random_data()
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+    main()
+import random
+
+        print(f"Random Number: {item}")
+def generate_random_data():
+    for item in data:
+
+def main():
+
+if __name__ == "__main__":
