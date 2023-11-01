d4568912f47f57d05e7a4248fba35808c4af0e71
Burty11 <142454463+Burty11@users.noreply.github.com>
2023-11-01 06:50:09 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,51 @@
+    main()
+    return data
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+def main():
+
+if __name__ == "__main__":
+import random
+
+def generate_random_data():
+
+    data = generate_random_data()
+        }
+
+    }
+        }
+    public static void main(String[] args) {
+}
+public class RandomDataGenerator {
+
+            System.out.println("Random Number: " + item);
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+        for (int item : data) {
+            data[i] = (int) (Math.random() * 100) + 1;import random
+    main()
+
+
+    return data
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+def generate_random_data():
+    for item in data:
+
+
+def main():        }
+            data[i] = (int) (Math.random() * 100) + 1;
+
+        int[] data = new int[10];
+        }
+public class RandomDataGenerator {
+
+            System.out.println("Random Number: " + item);
+}
+    public static void main(String[] args) {
+        for (int item : data) {
+        for (int i = 0; i < 10; i++) {
+    }
